af8bf1df00a0b03d9ae39818bb0f3c31f98ec4f9
Guo Zhenyu <gzy_988@126.com>
2021-03-21 13:18:23 +0800

    Add weight to overlap region, eliminate margin effect.

--- dataset/OverlapCrop.py
+++ dataset/OverlapCrop.py
@@ -53,29 +53,37 @@ class OverlapCrop():
 
     def _hpad(self, arr_l, arr_r, overlap=10):
         if arr_l.size == 1:
-            return arr_r
+            return arr_r.astype(np.float32)
         h, w, c = arr_l.shape
         patch_w = arr_r.shape[1]
+        if overlap == 10:
+            weight = np.array([0.03, 0.08, 0.15, 0.25, 0.5, 0.5, 0.75, 0.85, 0.92, 0.97])
+            weight = np.expand_dims(weight, axis=0)
+            weight = np.expand_dims(weight, axis=2)
         result = np.zeros((h, w + patch_w - overlap, c))
         result[:, :w-overlap, :] = arr_l[:, :w-overlap, :]
-        # tmp = np.round((arr_l[:, -overlap:, :] + arr_r[:, :overlap, :]) / 2)
+        tmp = arr_l[:, -overlap:, :] * (1-weight) + arr_r[:, :overlap, :] * weight
         # tmp = arr_l[:, -overlap:, :]
-        tmp = arr_r[:, :overlap, :]
-        result[:, w-overlap:w, :] = tmp.astype(arr_l.dtype)
+        # tmp = arr_r[:, :overlap, :]
+        result[:, w-overlap:w, :] = tmp
         result[:, w:, :] = arr_r[:, overlap:, :]
         return result
 
     def _vpad(self, arr_up, arr_down, overlap=10):
         if arr_up.size == 1:
-            return arr_down
+            return arr_down.astype(np.float32)
         h, w, c = arr_up.shape
         pathch_h = arr_down.shape[0]
+        if overlap == 10:
+            weight = np.array([0.03, 0.08, 0.15, 0.25, 0.5, 0.5, 0.75, 0.85, 0.92, 0.97])
+            weight = np.expand_dims(weight, axis=1)
+            weight = np.expand_dims(weight, axis=2)
         result = np.zeros((h + pathch_h - overlap, w, c))
         result[:h-overlap, :, :] = arr_up[:h-overlap, :, :]
-        # tmp = np.round((arr_up[-overlap:, :, :] + arr_down[:overlap, :, :]) / 2)
+        tmp = arr_up[-overlap:, :, :] * (1-weight) + arr_down[:overlap, :, :] * weight
         # tmp = arr_up[-overlap:, :, :]
-        tmp = arr_down[:overlap, :, :]
-        result[h-overlap:h, :, :] = tmp.astype(arr_up.dtype)
+        # tmp = arr_down[:overlap, :, :]
+        result[h-overlap:h, :, :] = tmp
         result[h:, :, :] = arr_down[overlap:, :, :]
         return result
 
@@ -121,18 +129,19 @@ class OverlapCrop():
 
 if __name__ == "__main__":
     # img_path = "../test/female_281.jpg"
-    img_path = "../test/imagenet.png"
+    img_path = "../results/0_0_in.png"
 
-    # img = cv2.imread(img_path)
-    # input = OverlapCrop(img, patch_size=48, overlap=10)
-    # patches = input.unfold()
+    img = cv2.imread(img_path)
+    input = OverlapCrop(img, patch_size=48, overlap=10)
+    patches = input.unfold()
 
     
-    # out_img = np.zeros(img.shape * 2)
-    # output = OverlapCrop(out_img, patch_size=48, overlap=10)
-    # output.set_patches(patches)
-    # out_img = output.get_image()
+    out_img = np.zeros(img.shape)
+    output = OverlapCrop(out_img, patch_size=48, overlap=10)
+    output.set_patches(patches)
+    output.fold()
+    out_img = output.get_image()
+    print(out_img[:5, :5, :1])
 
-    test(img_path)
-
+    cv2.imwrite("./test.png", out_img.astype(np.uint8))
     

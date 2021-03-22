05873e9301e4e39e47e56445752566925a2764d0
lizhexin <lizhexin_bit@163.com>
2021-03-22 22:25:39 +0800

    delete useless files.

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --seed 0 \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 python main.py --seed 0 \
 --data /data1/share/remote_sensing/patched_images/rgb_superview \
 --eval-data /data1/share/remote_sensing/patched_images/rgb_superview \
---batch-size 320 \
+--batch-size 256 \
 --eval \
 --resume /home/lizhexin/deit/ckpt/dehaze/checkpoint.pth.tar \
 --task "SRx3" \

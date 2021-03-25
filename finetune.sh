17c46b9517c37b2da7631af2a49cd05bd61575f9
lizhexin <lizhexin_bit@163.com>
2021-03-25 10:46:44 +0800

    Change default hyper parameters

--- finetune.sh
+++ finetune.sh
@@ -1,11 +1,9 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --seed 0 \
+python main.py --seed 0 \
 --lr 2e-5 \
 --save-path "./ckpt" \
 --epochs 30 \
---data /data1/share/remote_sensing/patched_images/rgb_superview \
---batch-size 320 \
---resume /home/lizhexin/deit/ckpt/dehaze/checkpoint.pth.tar \
---task "dehaze" \
-| tee ./logs/dehaze_bs320_lr2e-5_30epoch.log
-
+--data path-to-data \
+--batch-size 256 \
+--resume path-to-checkpoint \
+--task "dehaze"
 # finetune option "denoise30", "denoise50", "SRx2", "SRx3", "SRx4", "dehaze"
